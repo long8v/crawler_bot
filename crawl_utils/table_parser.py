247ce9c50b37b6ef69249e393be9de852482581d
long8v <ckleckle@gmail.com>
2020-03-05 17:34:16 +0900

    wish this table parser final

--- crawl_utils/table_parser.py
+++ crawl_utils/table_parser.py
@@ -15,9 +15,9 @@ def get_row(row):
     element = []
     for r in row:
         if r.has_attr('colspan'):
-            element += [r for _ in range(int(r['colspan']))]
+            element += [strip_all(r.text) for _ in range(int(r['colspan']))]
         else:
-            element += [r]
+            element += [strip_all(r.text)]
     return element
 
 def get_table_element(dom):
@@ -38,7 +38,7 @@ def get_template(row):
     template = []
     for r in row:
         if r.has_attr('rowspan'):
-            template += [r.text]
+            template += [strip_all(r.text)]
         elif r.has_attr('colspan'):
             template += [None for _ in range(int(r["colspan"]))]
         else:
@@ -60,7 +60,7 @@ def merge_template(template, row):
             try:
                 merged += [row.pop(0)]
             except:
-                print('merged:',merged)
+                pass
     return merged
     
 
@@ -69,22 +69,24 @@ def tbody_parsing(rows):
     input : nested list(# of columns, # of rows)
     output : 
     '''
+    element = []
     element_list = []
-    standard_len = len(get_table_element(rows[0]))
-    template = get_template(get_table_element(rows[0]))
-    for row in rows:
-        emnt = get_table_element(row)
-        if emnt:
-            if len(emnt) == standard_len:
-                template = get_template(emnt)
-                element = get_row(emnt)
-            else:
-                element = merge_template(template, get_row(emnt))
-        element_list.append(element)
-    return element_list
+    if rows:
+        standard_len = max(len(get_table_element(r)) for r in rows)
+        template = get_template(get_table_element(rows[0]))
+        for row in rows:
+            emnt = get_table_element(row)
+            if emnt:
+                if len(emnt) == standard_len:
+                    template = get_template(emnt)
+                    element = get_row(emnt)
+                else:
+                    element = merge_template(template, get_row(emnt))
+            element_list.append(element)
+        return element_list
 
 
-def get_table_column_thead(table):
+def get_table_column(table):
     '''
     input : table(dom obejct)
     output : column(list)
@@ -98,16 +100,15 @@ def get_table_column_thead(table):
     if len(table.find_all('tr')) > 1:
         for tr in trs:
             columns.append(tr.find_all(['th', 'td']))
-        first_row = get_template(columns[1])
-        second_row = [strip_all(c.text) for c in columns[1]]
-        final_row = merge_template(first_row, second_row)
+        first_row = get_template(columns[0])
+        if any(first_row):
+            second_row = [strip_all(c.text) for c in columns[1]]
+            return (merge_template(first_row, second_row), 2)
+            
+        else:
+            return (get_row(columns[0]), 1)
     else:
-        final_row = [strip_all(th.text) for th in table.find_all(['th'])]
-    return final_row
-
-def get_table_column_tbody(table):
-    trs = table.find_all('tr')
-    return [strip_all(c.text) for c in get_row(get_table_element(trs[0]))]
+        return (get_row(trs[0].find_all(['th', 'td'])), 1)
 
 def table_parsing(url):
     '''
@@ -123,46 +124,21 @@ def table_parsing(url):
     soup = parsing(url)
     if soup:
         tables = soup.find_all('table')
-        print(len(tables))
         for table in tables:
             table_df = pd.DataFrame()
-            table_head = table.find('thead')
-            table_body = table.find('tbody')
-            if table_body:
-                columns_body = get_table_column_tbody(table_body)
-                rows = table_body.find_all('tr')
-                element_list = tbody_parsing(rows)
-                element = element_list[0]
-            if table_head:
-                columns_head = get_table_column_thead(table_head)
-                if len(element) == len(columns_head):
-                    columns = columns_head
-            elif len(element) == len(columns_body):
-                columns = columns_body
+            columns_body, n = get_table_column(table)
+            rows = table.find_all('tr')
+        
+            element_list = tbody_parsing(rows)
+            columns, n = get_table_column(table)
+            len_element = max(len(e) for e in element_list)
+            for _ in range(n):
                 element_list.pop(0)
             if not columns:
-                columns = [str(_) for _ in range(len(element))]
+                columns = [str(_) for _ in range(len_element)]
             columns = list(map(strip_all, columns))
             for key, value in zip(columns, zip(*element_list)):
-                table_df[key] = [v if type(v) is str else " ".join(list(v)) for v in list(value)]         
+                table_df[key] = [v for v in list(value)]         
             table_df_list.append(table_df)
     table_df_list = [table for table in table_df_list if any(table)]
     return table_df_list
-
-
-
-def get_table_list(df):
-    '''
-    input : dataframe having full_url
-    output : list of tables(dataframe)
-    
-    using table parser, get list of tables given dataframe with full_url 
-    '''
-    table_list = []
-    for _ in df["full"][:]:
-        table = table_parsing(get_html_text(_))
-        if table:
-            table_list += [table]
-    return table_list
-
-
